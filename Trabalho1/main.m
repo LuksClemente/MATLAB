dc02d9c0a11c86bdc565955c45cd22d5b0d456e1
Lucas Clemente <43150252+LuksClemente@users.noreply.github.com>
2019-10-31 13:14:34 -0300

    Update main.m

--- Trabalho1/main.m
+++ Trabalho1/main.m
@@ -2,9 +2,9 @@ clc;
 clear;
 close all;
 
-tol = 1.0e-2;
-maxiter = 100;
-x0 = [2; 2];
+tol = 1.0e-4;
+maxiter = input("Digite um numero maximo de iteracoes: ");
+x0 = input("Determine o chute inicial: ");
 F = @(x)[x(1)^2 + x(2)^2 - 2; x(1)^2 - x(2)^2 - 1];
-DF = @(x)[2*x(1) 2*x(2); 2*x(1) -2*x(2)];
+DF = @(x)[2*x(1), 2*x(2); 2*x(1), -2*x(2)];
 x = NewtonRaphson(F, DF, x0, tol, maxiter);
